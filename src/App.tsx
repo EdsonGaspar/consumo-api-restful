173cfd2ac333229b58957eed85d5218eafb98436
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2024-08-28 21:41:23 +0100

    Consumindo API usando o UseEffect, but doesn't worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+type Repository = {
+  full_name: string;
+  description: string;
+};
+
 function App() {
-  return <div></div>;
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((data) => {
+        setRepositories(data);
+      });
+  }, []);
+  return (
+    <ul>
+      {repositories.map((repo) => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default App;
